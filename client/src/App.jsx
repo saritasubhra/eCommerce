6c0440eb8651d9ec599593ed1b6f2b999e36aa1c
saritasubhra <saritasubhra1@gmail.com>
2025-01-17 10:14:59 +0530

    signup & login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,26 @@ import { Toaster } from "react-hot-toast";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="p-4 h-screen flex items-center justify-center bg-slate-900">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
+      <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
+        <div className="absolute inset-0 overflow-hidden">
+          <div className="absolute inset-0">
+            <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(16,185,129,0.3)_0%,rgba(10,80,60,0.2)_45%,rgba(0,0,0,0.1)_100%)]" />
+          </div>
+        </div>
+
+        <div className="relative z-50 pt-20">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </div>
         <Toaster />
       </div>
     </BrowserRouter>
